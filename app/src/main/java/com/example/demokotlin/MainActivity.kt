026fde3cf28afd1bb85db79bdbeffa558126bc04
PandaMinh <nguyenthiminh22122004@gmail.com>
2023-04-08 10:53:19 +0700

    layout

--- app/src/main/java/com/example/demokotlin/MainActivity.kt
+++ app/src/main/java/com/example/demokotlin/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.demokotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.demokotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+//        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.login.setOnClickListener {
+            when (binding.password.transformationMethod) {
+                PasswordTransformationMethod.getInstance() -> {
+                    binding.password.transformationMethod =
+                        HideReturnsTransformationMethod.getInstance()
+                }
+                HideReturnsTransformationMethod.getInstance() -> {
+                    binding.password.transformationMethod =
+                        PasswordTransformationMethod.getInstance()
+                }
+            }
+        }
     }
 }
