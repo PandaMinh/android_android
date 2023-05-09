97de33c971e86beda81c98ac287c0063e096931b
PandaMinh <nguyenthiminh22122004@gmail.com>
2023-05-09 08:57:11 +0700

    9/5/2023

--- app/src/main/java/com/example/demokotlin/MainActivity.kt
+++ app/src/main/java/com/example/demokotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.demokotlin
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
