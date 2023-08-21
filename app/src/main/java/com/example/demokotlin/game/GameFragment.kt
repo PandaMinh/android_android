977385d88b3e3f6932fbf6c98fa323317c2c2e6b
PandaMinh <nguyenthiminh22122004@gmail.com>
2023-08-21 11:19:32 +0700

    LiveData

--- app/src/main/java/com/example/demokotlin/game/GameFragment.kt
+++ app/src/main/java/com/example/demokotlin/game/GameFragment.kt
@@ -1,10 +1,10 @@
 package com.example.demokotlin.game
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.demokotlin.databinding.FragmentGameBinding
 
@@ -15,32 +15,42 @@ class GameFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentGameBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
         binding.buttonGameFragmentReset.setOnClickListener {
-            viewModel.reset(
-                binding.textViewGameFragmentWatermelonScore.text.toString(),
-                binding.textViewGameFragmentDurianScore.text.toString()
-            )
-            viewModel.show(
-                binding.textViewGameFragmentWatermelonScore = viewModel.scoreWatermelon,
-                binding.textViewGameFragmentDurian = viewModel.scoreDurian
-            )
+            viewModel.reset()
         }
+
         binding.buttonGameFragmentScore1Watermelon.setOnClickListener {
-            viewModel.incrementScore(
-                binding.textViewGameFragmentWatermelonScore.text.toString(),
-                binding.textViewGameFragmentDurianScore.text.toString()
-            )
-            viewModel.show(
-                binding.textViewGameFragmentWatermelonScore = viewModel.scoreWatermelon,
-                binding.textViewGameFragmentDurian = viewModel.scoreDurian
-            )
+            viewModel.incrementScore(true, 1)
+        }
+
+        binding.buttonGameFragmentScore2Watermelon.setOnClickListener {
+            viewModel.incrementScore(true, 2)
+        }
+
+        binding.buttonGameFragmentScore3Watermelon.setOnClickListener {
+            viewModel.incrementScore(true, 3)
+        }
+
+        binding.buttonGameFragmentScore1Durian.setOnClickListener {
+            viewModel.incrementScore(false, 1)
+        }
+
+        binding.buttonGameFragmentScore2Durian.setOnClickListener {
+            viewModel.incrementScore(false, 2)
+        }
+
+        binding.buttonGameFragmentScore3Durian.setOnClickListener {
+            viewModel.incrementScore(false, 3)
         }
     }
 }
--- app/src/main/java/com/example/demokotlin/game/GameViewModel.kt
+++ app/src/main/java/com/example/demokotlin/game/GameViewModel.kt
@@ -1,27 +1,32 @@
 package com.example.demokotlin.game
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GameViewModel: ViewModel() {
-    var scoreWatermelon : Int = 0
-    var scoreDurian : Int = 0
+class GameViewModel : ViewModel() {
+    private val _scoreWatermelon = MutableLiveData(0)
+    val scoreWatermelon: LiveData<Int> get() = _scoreWatermelon
+//    var scoreDurian : Int = 0
+//        private set
 
-    fun incrementScore( isWatermelon: Boolean){
-        if( isWatermelon){
-            scoreWatermelon++
-        }
-        else{
-            scoreDurian++
+    private val _scoreDurian = MutableLiveData(0)
+    val scoreDurian: LiveData<Int> get() = _scoreDurian
+
+    val team1Name = MutableLiveData("Watermelon")
+    val team2Name = MutableLiveData("Durian")
+
+    fun incrementScore(isWatermelon: Boolean, score: Int) {
+        if (isWatermelon) {
+            _scoreWatermelon.value = scoreWatermelon.value?.plus(score)
+        } else {
+            //_scoreDurian += score
+            _scoreDurian.value = scoreDurian.value?.plus(score)
         }
     }
 
-    fun reset(){
-        scoreWatermelon = 0
-        scoreDurian = 0
-    }
-
-    fun show(){
-        print(scoreWatermelon)
-        print(scoreDurian)
+    fun reset() {
+        _scoreWatermelon.value = 0
+        _scoreDurian.value = 0
     }
 }
